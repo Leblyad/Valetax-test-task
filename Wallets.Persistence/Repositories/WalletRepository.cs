860364f9414bfbe95dfefda53784fbf9742712b9
Kiryl Lebedzeu <apk59955@gmail.com>
2026-09-23 21:19:28 +0300

    Fix balance update

--- Wallets.Persistence/Repositories/WalletRepository.cs
+++ Wallets.Persistence/Repositories/WalletRepository.cs
@@ -17,4 +17,14 @@ public class WalletRepository : RepositoryBase<Wallet>, IWalletRepository
         CancellationToken cancellationToken = default) =>
         FindByCondition(w => w.UserExternalId == userExternalId, trackChanges)
             .SingleOrDefaultAsync(cancellationToken);
+
+    public Task IncrementBalanceAsync(
+        Guid userExternalId,
+        decimal amount,
+        CancellationToken cancellationToken = default) =>
+        RepositoryContext.Set<Wallet>()
+            .Where(w => w.UserExternalId == userExternalId)
+            .ExecuteUpdateAsync(
+                setters => setters.SetProperty(w => w.Balance, w => w.Balance + amount),
+                cancellationToken);
 }
