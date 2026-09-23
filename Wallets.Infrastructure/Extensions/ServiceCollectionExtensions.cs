3cc7a1a2d0a2ed9d126dd9cf6166fd5a2c371f84
Kiryl Lebedzeu <apk59955@gmail.com>
2026-09-23 20:47:19 +0300

    Fix relations bug, add cb for polly

--- Wallets.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Wallets.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -12,6 +12,10 @@ namespace Wallets.Infrastructure.Extensions;
 public static class ServiceCollectionExtensions
 {
     private const int HTTP_RETRY_ATTEMPTS = 3;
+    private const double CIRCUIT_BREAKER_FAILURE_RATIO = 0.5;
+    private const int CIRCUIT_BREAKER_MINIMUM_THROUGHPUT = 5;
+    private const int CIRCUIT_BREAKER_SAMPLING_SECONDS = 30;
+    private const int CIRCUIT_BREAKER_BREAK_SECONDS = 30;
 
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
@@ -41,6 +45,13 @@ public static class ServiceCollectionExtensions
                     Delay = TimeSpan.FromSeconds(1),
                     BackoffType = DelayBackoffType.Exponential,
                     UseJitter = true,
+                });
+                builder.AddCircuitBreaker(new HttpCircuitBreakerStrategyOptions
+                {
+                    SamplingDuration = TimeSpan.FromSeconds(CIRCUIT_BREAKER_SAMPLING_SECONDS),
+                    FailureRatio = CIRCUIT_BREAKER_FAILURE_RATIO,
+                    MinimumThroughput = CIRCUIT_BREAKER_MINIMUM_THROUGHPUT,
+                    BreakDuration = TimeSpan.FromSeconds(CIRCUIT_BREAKER_BREAK_SECONDS),
                 });
                 builder.AddTimeout(TimeSpan.FromSeconds(timeoutSeconds));
             });
